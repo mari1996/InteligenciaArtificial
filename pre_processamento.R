515efbc3eb545ead595b4a87459ed3d5335c91ea
Mariana Viana <mariana.viana@usp.br>
2018-04-15 21:57:24 -0300

    Update pre_processamento.R
    Agora com matriz transposta

--- pre_processamento.R
+++ pre_processamento.R
@@ -1,59 +1,62 @@
-#Representações por frequência, binária e tfidf
+#representacoes por frequencia, binaria e tfidf
 
-#é necessário rodar os comandos abaixo antes de continuar, para que as bibliotecas usadas sejam instaladas (se ainda não estiverem instaladas)
+#e necessario rodar os comandos abaixo antes de continuar, para que as bibliotecas usadas sejam instaladas (se ainda 
+#nao estiverem instaladas)
 #install.packages('tm')
 #install.packages('SnowballC')
 #install.packages('stringr')
 
 #biblioteca usada especificamente para text-mining
 library(tm)
-#biblioteca que disponibiliza o uso do stemming (redução de palavras para o seu radical)
+#biblioteca que disponibiliza o uso do stemming (reducao de palavras para o seu radical)
 library(SnowballC)
-#biblioteca que disponibiliza funções para manipulação de strings
+#biblioteca que disponibiliza funcoes para manipualacao de strings
 library(stringr)
 
-#captura o diretório
-#Nesse caso, para rodar, é necessário alterar o diretório para o local onde estão os textos no seu computador
-#Pega todos os textos que estão nesse diretório e transforma num corpus
-CorpusVar <- Corpus(DirSource("C:/Users/Mariana/Desktop/IA/teste_R"))
+#captura o diretorio
+#Nesse caso, para rodar, e necessario alterar o diretorio para o local onde estao os textos no seu computador
+#Pega todos os textos que estao nesse diretorio e transforma num corpus
+CorpusVar <- Corpus(DirSource("C:/Users/Mariana/Desktop/IA/Textos/Entrada1/Olar"))
 
-#deixa todas as letras em minúsculo
+
+#deixa todas as letras em minusculo
 CorpusVar <- tm_map(CorpusVar, content_transformer(tolower))
 
-#remove pontuação dos textos
+#remove pontuacao dos textos
 CorpusVar <- tm_map(CorpusVar, removePunctuation)
 
-#remove os números dos textos
+#remove os numeros dos textos
 CorpusVar <- tm_map(CorpusVar, removeNumbers)
 
-#retira as stop words (língua inglesa)
+#retira as stop words (lingua inglesa)
 CorpusVar <- tm_map(CorpusVar, removeWords, stopwords("english"))
 
-#remove espaços em branco que são desnecessários
+#remove espacos em branco que sao desnecessarios
 CorpusVar <- tm_map(CorpusVar, stripWhitespace)
 
-#reduz as palavras para o seu radical (língua inglesa)
+#reduz as palavras para o seu radical (lingua inglesa)
 CorpusVar <- tm_map(CorpusVar, stemDocument, language="english")
 
-#A primeira representação é a por frequência
-#transforma o corpus em uma matriz de frequência
+#A primeira representacao e a por frequencia
+#transforma o corpus em uma matriz de frequencia
 tdm <- TermDocumentMatrix(CorpusVar)
 #converte pra matriz
 m <- as.matrix(tdm)
-#colocar pasta específica de saída com o nome do arquivo
-write.csv(m, file="C:/Users/Mariana/Desktop/IA/Saidas_teste/Process_Mining_Abstracts_TF.csv")
+transp <- t(m)
+write.csv(transp, file="C:/Users/Mariana/Desktop/IA/Saidas_teste/Process_Mining_Abstracts_TF.csv")
 
-#A segunda representação é a binária
+#A segunda representacao e a binaria
 m <- weightBin(tdm)
 #converte pra matriz
-m2 <- as.matrix(m)
-#colocar pasta específica de saída com o nome do arquivo
+transp <- t(m)
+m2 <- as.matrix(transp)
 write.csv(m2, file="C:/Users/Mariana/Desktop/IA/Saidas_teste/Process_Mining_Abstracts_Binario.csv")
 
-#A terceira representação é a TFIDF
+#A terceira representacao e a TFIDF
 terms <-DocumentTermMatrix(CorpusVar,control = list(weighting = function(x) weightTfIdf(x, normalize = FALSE)))
 #converte pra matriz
 m3 <- as.matrix(terms)
-#colocar pasta específica de saída com o nome do arquivo                           
+transp <- t(m3)
+matriz <- as.matrix(transp)
 write.csv(m3, file="C:/Users/Mariana/Desktop/IA/Saidas_teste/TFIDF.csv")
 
